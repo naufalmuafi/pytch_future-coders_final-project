b57f86e4ea2c682d263bf8139b1a3c2bd943f673
Naufal Muafi <nmuafi1@gmail.com>
2024-10-22 20:40:21 +0700

    add: group 2 done

--- 2_gyan_beggar_project/main.py
+++ 2_gyan_beggar_project/main.py
@@ -4,13 +4,14 @@ import random
 score = 0
 move_size = 20
 
+
 class Bowl(pytch.Sprite):
     Costumes = ["bowl.png"]
-    
+
     @pytch.when_green_flag_clicked
     def main(self):
         self.go_to_xy(0, -145)
-    
+
     @pytch.when_key_pressed("ArrowLeft")
     def move_left(self):
         self.change_x(-move_size)
@@ -19,17 +20,23 @@ class Bowl(pytch.Sprite):
     def move_right(self):
         self.change_x(move_size)
 
+
 class Money(pytch.Sprite):
     Costumes = ["money.jpg"]
-    
+
     def drop(self, x, y):
         self.go_to_xy(x, y)
         global score
-        while self.y_position > -140:
+
+        # topik pemrograman 2: looping for
+        # Using a for loop to simulate the falling motion
+        for _ in range(y, -140, random.randint(-6, -3)):
             self.change_y(random.randint(-6, -3))
+            # topik pemrograman 1: if
             if self.touching(Bowl):
                 self.hide()
                 score += 1
+                break  # Stop the loop if it touches the bowl
 
     @pytch.when_green_flag_clicked
     def set_position(self):
@@ -37,9 +44,10 @@ class Money(pytch.Sprite):
             self.show()
             self.drop(random.randint(-145, 190), random.randint(120, 150))
 
+
 class City(pytch.Stage):
     Backdrops = ["city.jpg"]
-    
+
     @pytch.when_green_flag_clicked
     def main(self):
         pytch.show_variable(None, "score")
